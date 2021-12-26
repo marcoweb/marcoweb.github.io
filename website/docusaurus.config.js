fdaa24396236bb78d3b1272e41ca4f6945f0e908
Marco Carvalho <marcocarvalho.web@gmail.com>
2021-12-26 17:02:33 -0300

    Home

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -48,65 +48,65 @@ const config = {
           alt: 'My Site Logo',
           src: 'img/MW.png',
         },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
+      //   items: [
+      //     {
+      //       type: 'doc',
+      //       docId: 'intro',
+      //       position: 'left',
+      //       label: 'Tutorial',
+      //     },
+      //     {to: '/blog', label: 'Blog', position: 'left'},
+      //     {
+      //       href: 'https://github.com/facebook/docusaurus',
+      //       label: 'GitHub',
+      //       position: 'right',
+      //     },
+      //   ],
+      // },
       footer: {
         style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        // links: [
+        //   {
+        //     title: 'Docs',
+        //     items: [
+        //       {
+        //         label: 'Tutorial',
+        //         to: '/docs/intro',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'Community',
+        //     items: [
+        //       {
+        //         label: 'Stack Overflow',
+        //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //       },
+        //       {
+        //         label: 'Discord',
+        //         href: 'https://discordapp.com/invite/docusaurus',
+        //       },
+        //       {
+        //         label: 'Twitter',
+        //         href: 'https://twitter.com/docusaurus',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'More',
+        //     items: [
+        //       {
+        //         label: 'Blog',
+        //         to: '/blog',
+        //       },
+        //       {
+        //         label: 'GitHub',
+        //         href: 'https://github.com/facebook/docusaurus',
+        //       },
+        //     ],
+        //   },
+        // ],
+        copyright: `Copyright © ${new Date().getFullYear()} Marco Carvalho. Desenvolvido com Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
--- website/src/components/HomepageFeatures.js
+++ website/src/components/HomepageFeatures.js
@@ -4,32 +4,29 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Notas de Estudo',
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Artigos sobre meus estudos em tecnologia, em especial, desenvolvimento de software
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Tutoriais',
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Exercícios práticos, passo a passo, sobre instalação de ferramentas e desenvolvimento de software.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Conteúdo para Alunos',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Conteúdos desenvolvidos para os alunos dos cursos presenciais onde ministro aulas, disponíveis a todos.
       </>
     ),
   },
